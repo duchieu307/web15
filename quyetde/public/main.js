64225e9d7e853f8c144a18bd82e1f09496ddba8d
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-10-10 22:14:25 +0700

    A

--- quyetde/public/main.js
+++ quyetde/public/main.js
@@ -8,15 +8,42 @@
 // 	console.log(error);
 // });
 
-$.ajax({
-	url: "http://localhost:3000/randomquestion",
-	type: "GET",
-	success: function(response) {
-		if(response) {
-			$("#questionContent").text(response.questionContent);
+function getRandomQuestion() {
+	$.ajax({
+		url: "http://localhost:3000/randomquestion",
+		type: "GET",
+		success: function(response) {
+			if(response) {
+				$("#questionContent").text(response.questionContent);
+				$(".answer_btn").data("questionid", response.id);
+			}
+		},
+		error: function(err) {
+			console.log(err);
 		}
-	},
-	error: function(err) {
-		console.log(err);
-	}
+	});
+}
+
+getRandomQuestion();
+
+$("#otherQuestion").on("click", function() {
+	getRandomQuestion();
+});
+
+$(".answer_btn").on("click", function() {
+	console.log($(this).data())
+	$.ajax({
+		url: "http://localhost:3000/answer",
+		type: "POST",
+		data: $(this).data(),
+		success: function(response) {
+			if(response.success) {
+				window.location.href = "/";
+			}
+		},
+		error: function(err) {
+			console.log(err);
+			window.location.href = "https://google.com/search?q=site:stackoverflow.com why my code didn't run";
+		}
+	})
 });
