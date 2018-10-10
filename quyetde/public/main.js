03c83181972f3e46e153127d0e068fb505984fc2
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-10-10 20:44:13 +0700

    A

--- quyetde/public/main.js
+++ quyetde/public/main.js
@@ -1,19 +1,22 @@
-// const maxLength = 200;
-// const questionContentElem = document.getElementById('questionContent');
-// const remainCharElem = document.getElementById('remain');
-
-// questionContentElem.addEventListener(
-// 	'input',
-// 	function() {
-// 		var remainChar = maxLength - questionContentElem.value.length;
-// 		remainCharElem.innerText = remainChar;
+// axios.get("http://localhost:3000/randomquestion")
+// .then(function (response) {
+//     if(response.data) {
+// 		document.getElementById("questionContent").innerText = response.data.questionContent;
 // 	}
-// );
+// })
+// .catch(function (error) {
+// 	console.log(error);
+// });
 
-axios.get("http://localhost:3000/randomquestion")
-.then(function (response) {
-    console.log(response.data);
-})
-.catch(function (error) {
-	console.log(error);
+$.ajax({
+	url: "http://localhost:3000/randomquestion",
+	type: "GET",
+	success: function(response) {
+		if(response) {
+			$("#questionContent").text(response.questionContent);
+		}
+	},
+	error: function(err) {
+		console.log(err);
+	}
 });
